adff213cb1f8852379e826b0ce3337b6b2965f29
kPaNN8874 <113461676+kPaNN8874@users.noreply.github.com>
2022-11-06 20:19:17 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function plusz() {
+    document.szamol.eredmeny.value = document.szamol.Beker_egy.value *1 + document.szamol.Beker_ketto.value * 1;
+    }
+    function minusz() {
+    document.szamol.eredmeny.value = document.szamol.Beker_egy.value - document.szamol.Beker_ketto.value;
+    }
+    function szor() {
+    document.szamol.eredmeny.value = document.szamol.Beker_egy.value * document.szamol.Beker_ketto.value;
+    }
+    function oszt() {
+    document.szamol.eredmeny.value = document.szamol.Beker_egy.value / document.szamol.Beker_ketto.value;
+    }
